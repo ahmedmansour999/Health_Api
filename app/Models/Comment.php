70195743ad6aba844077f6a1163878fc99e1f402
Ahmed Maky <129126532+ahmedmansour999@users.noreply.github.com>
2024-03-12 16:44:59 +0200

    migration

---
+++ app/Models/Comment.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+
+    // retlation one to many with Doctor
+
+    public function doctor(){
+
+        return $this->belongsTo(Doctor::class);
+
+    }
+
+
+    // retlation one to many with patient
+
+    public function patient(){
+
+        return $this->belongsTo(Patient::class);
+
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CommentController;
+use App\Models\Comment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('comments', CommentController::class);
+
+
+/*
+
+GET|HEAD        api/comments ....... comments.index › App\Models\Comment@index
+  POST            api/comments ....... comments.store › App\Models\Comment@store
+  GET|HEAD        api/comments/{comment} ......... comments.show › App\Models\Comment@show
+  PUT|PATCH       api/comments/{comment} ..... comments.update › App\Models\Comment@update
+  DELETE          api/comments/{comment} ... comments.destroy › App\Models\Comment@destroy
+
+
+
+*/

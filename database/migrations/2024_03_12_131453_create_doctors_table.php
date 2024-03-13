11629540742a42afc3af47b71e86e94db237b4f4
Ahmed Maky <129126532+ahmedmansour999@users.noreply.github.com>
2024-03-13 04:07:32 +0200

    edit

--- database/migrations/2024_03_12_131453_create_doctors_table.php
+++ database/migrations/2024_03_12_131453_create_doctors_table.php
@@ -13,10 +13,8 @@ return new class extends Migration
     {
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('specialty')->nullable();
-            $table->string('hospital')->nullable();
-            $table->text('description')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
             $table->timestamps();
         });
     }
--- database/migrations/2024_03_12_131941_create_patients_table.php
+++ database/migrations/2024_03_12_131941_create_patients_table.php
@@ -13,11 +13,8 @@ return new class extends Migration
     {
         Schema::create('patients', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone')->nullable();
-            $table->date('dob')->nullable();
-            $table->text('address')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
             $table->timestamps();
         });
     }
